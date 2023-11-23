1a6d1380cfb22e217ff4546a49813fc87b854c9d
m6tm <maboadaniel.55@gmail.com>
2023-11-23 06:31:34 +0100

    dotenv ajouté et la génération des méta données aussi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,28 @@
+import type { Metadata, ResolvingMetadata } from 'next'
 
+type Props = {
+        params: { id: string }
+        searchParams: { [key: string]: string | string[] | undefined }
+}
 
-export default function Home() {
+export async function generateMetadata(
+        { params, searchParams }: Props,
+        parent: ResolvingMetadata
+): Promise<Metadata> {
+        const product = {
+                title: `${process.env.APP_NAME} - Home`
+        }
+
+        const previousImages = (await parent).openGraph?.images || []
+
+        return {
+                ...product,
+                openGraph: {
+                        images: ['/logo.jpg', ...previousImages],
+                },
+        }
+}
+
+export default function Page({ params, searchParams }: Props) {
         return <h1>Welcome home !!!</h1>
 }
